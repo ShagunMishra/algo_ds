09a74f84dda0ba0f9c0c6609b993801ef1963d39
Shagun Mishra <69380085+ShagunMishra@users.noreply.github.com>
2022-02-23 15:36:09 +0530

    Add files via upload

---
+++ 5.Recursion/Mydebug/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum=0;
+
+    int A[]={1,2,3,4,5};
+
+    for(auto x:A)
+    {
+        sum=sum+x;
+    }
+
+    cout<<sum;
+    return 0;
+}
