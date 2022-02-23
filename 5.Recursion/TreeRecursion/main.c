8bcad24c9c6509e021c7f8d08f2c4754dc34cbe8
Shagun Mishra <69380085+ShagunMishra@users.noreply.github.com>
2022-02-23 15:37:34 +0530

    Add files via upload

---
+++ 5.Recursion/TreeRecursion/main.c
@@ -0,0 +1,20 @@
+//tree recursion
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void fun(int n)
+{
+    if(n>0)
+    {
+        printf("%d",n);
+        fun(n-1);
+        fun(n-1);
+    }
+}
+
+int main()
+{
+    fun(3);
+    return 0;
+}
